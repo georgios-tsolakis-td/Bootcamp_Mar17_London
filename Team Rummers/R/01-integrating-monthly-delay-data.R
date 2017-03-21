bbff4a410e3ee93ff3e7fc965c6ea38aa39a3961
Christoph Waldhauser <christoph.waldhauser@thinkbiganalytics.com>
2017-03-21 16:40:33 +0000

    Rummers: flight delay data integration

---
+++ Team Rummers/R/01-integrating-monthly-delay-data.R
@@ -0,0 +1,22 @@
+# Rummers' Flight Delays
+# Integrating delay data
+
+# This file integrates monthly flight delay data retrieved from
+# <https://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236>
+
+# The result is stored as RData and csv file.
+
+library(data.table)
+
+# (1) Read in raw csv data files ####
+fl1 <- fread('On_Time_On_Time_Performance_2016_1.csv')
+fl2 <- fread('On_Time_On_Time_Performance_2016_2.csv')
+fl3 <- fread('On_Time_On_Time_Performance_2016_3.csv')
+
+
+# (2) Bind monthly data together ####
+fl <- rbindlist(list(fl1, fl2, fl3))
+
+# (3) Save data to disk ####
+save(fl, file = 'flightdelays1-3.RData')
+write.csv(fl, file = 'flightdelays1-3.csv')
---
+++ Team Rummers/data/README.md
@@ -0,0 +1,11 @@
+# Rummers' Flight Delays
+
+## Flight delay data
+
+In this data, there lives data retrieved from <https://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236>.
+
+This is monthly flight delay data with approx 110 variables. We are using the
+first three months of 2016.
+
+## Note
+The data is not included in the git repo.
