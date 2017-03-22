d7266de0b19c24a6ae677abba9f0736bb8044057
Niket Doshi <niket.doshi@thinkbiganalytics.com>
2017-03-22 11:52:59 +0100

    plotbuildR demo

--- TechTrends-VisualisationScience/inst/plotbuildR-demo/app.R
+++ TechTrends-VisualisationScience/inst/plotbuildR-demo/app.R
@@ -7,42 +7,25 @@
 #    http://shiny.rstudio.com/
 #
 
+source("global.R")
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  sidebarLayout(
+    sidebarPanel = selectInput("species", label = "Select a Species", choices = data1$Species),
+    mainPanel = generatePlotUI(id = "test-plot", col_width = 12, status = "danger",solidHeader = T)
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  reactive_list <- list()
+  reactive_list$data_filtered <- reactive({
+     data1 %>% filter(Species %in% input$species)
+  })
+   callModule(module = generatePlot, id = "test-plot", df_list = df_list, reactive_df = reactive_list, df = "data_filtered")
 }
 
 # Run the application 
